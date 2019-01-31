1efd1ee856e9879a1559419bf80d2ea71696d3c0
Seunghyun Lee <Seunghyun Lee@DESKTOP-AJMVB78>
2019-02-01 01:48:48 +0900

    ..

---
+++ src/main/java/com/kr/stocky/TttttttApplication.java
@@ -0,0 +1,14 @@
+package com.kr.stocky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TttttttApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TttttttApplication.class, args);
+	}
+
+}
+
